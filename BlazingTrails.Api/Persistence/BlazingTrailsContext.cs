4cbd696364eb26853c6e36e863a52dd4e36f46ed
Sylvester Boniface <cutepoyzon@gmail.com>
2023-02-05 10:51:23 +0000

    Initial setup

---
+++ BlazingTrails.Api/Persistence/BlazingTrailsContext.cs
@@ -0,0 +1,24 @@
+﻿using BlazingTrails.Api.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingTrails.Api.Persistence
+{
+    public class BlazingTrailsContext : DbContext
+    {
+        public BlazingTrailsContext(DbContextOptions<BlazingTrailsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Trail> Trails => Set<Trail>();
+        public DbSet<RouteInstruction> RouteInstructions => Set<RouteInstruction>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new TrailConfig());
+            modelBuilder.ApplyConfiguration(new RouteInstructionConfig());
+        }
+    }
+}
